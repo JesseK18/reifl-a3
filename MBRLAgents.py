c4031d9362098846996b770840dd67150f87e424
Jesse <jessekroll2gmail.com>
2024-04-26 13:14:21 +0200

    sweeper done

--- MBRLAgents.py
+++ MBRLAgents.py
@@ -10,7 +10,9 @@ import numpy as np
 from queue import PriorityQueue
 from MBRLEnvironment import WindyGridworld
 
+
 class DynaAgent:
+
 
     def __init__(self, n_states, n_actions, learning_rate, gamma):
         self.n_states = n_states
@@ -22,7 +24,7 @@ class DynaAgent:
         self.means = np.zeros((n_states,n_actions))
         self.transition_counts = np.zeros((n_states,n_actions,n_states))
         self.reward_sums = np.zeros((n_states,n_actions,n_states))
-        
+       
     def select_action(self, s, epsilon):
         # TO DO: Change this to e-greedy action selection
         if np.random.rand() > epsilon:
@@ -30,7 +32,7 @@ class DynaAgent:
         else:
             a = np.random.choice(self.n_actions)
         return a
-        
+       
     def update(self,s,a,r,done,s_next,n_planning_updates):
         # TO DO: Add Dyna update
         self.transition_counts[s][a][s_next] += 1
@@ -45,13 +47,17 @@ class DynaAgent:
                 s = np.random.choice(self.n_states)
                 a = np.random.choice(self.n_actions)
 
+
             prob_transition = (self.transition_counts[s][a])/(np.sum(self.transition_counts[s][a]))
             s_next = np.random.choice(self.n_states, p=prob_transition)
             r = self.reward_sums[s][a][s_next]/self.transition_counts[s][a][s_next]
 
 
+
+
             self.Q_sa[s,a] += self.learning_rate * (r + self.gamma * np.max(self.Q_sa[s_next]) - self.Q_sa[s,a])
         pass
+
 
     def evaluate(self,eval_env,n_eval_episodes=30, max_episode_length=100):
         returns = []  # list to store the reward per episode
@@ -70,7 +76,9 @@ class DynaAgent:
         mean_return = np.mean(returns)
         return mean_return
 
+
 class PrioritizedSweepingAgent:
+
 
     def __init__(self, n_states, n_actions, learning_rate, gamma, max_queue_size=200, priority_cutoff=0.01):
         self.n_states = n_states
@@ -78,14 +86,15 @@ class PrioritizedSweepingAgent:
         self.learning_rate = learning_rate
         self.gamma = gamma
         self.priority_cutoff = priority_cutoff
-        
+       
         self.Q_sa = np.zeros((n_states,n_actions))
         # TO DO: Initialize count tables, reward sum tables, priority queue
-        self.means = np.zeros((n_states,n_actions))
+        #self.means = np.zeros((n_states,n_actions))
         self.transition_counts = np.zeros((n_states,n_actions,n_states))
         self.reward_sums = np.zeros((n_states,n_actions,n_states))
-        self.queue = PriorityQueue(maxsize=max_queue_size)
-        
+        self.queue = PriorityQueue()
+        #self.PQ = PriorityQueue()
+       
     def select_action(self, s, epsilon):
         # TO DO: Change this to e-greedy action selection
         if np.random.rand() > epsilon:
@@ -93,43 +102,75 @@ class PrioritizedSweepingAgent:
         else:
             a = np.random.choice(self.n_actions)
         return a
-        
+       
     def update(self,s,a,r,done,s_next,n_planning_updates):
-        
+       
         # TO DO: Add Prioritized Sweeping code
-        
+        #counter = 0
         # Helper code to work with the queue
         # Put (s,a) on the queue with priority p (needs a minus since the queue pops the smallest priority first)
-        # self.queue.put((-p,(s,a))) 
+        # self.queue.put((-p,(s,a)))
         # Retrieve the top (s,a) from the queue
         # _,(s,a) = self.queue.get() # get the top (s,a) for the queue
+        
+        # not working yet
+        # self.transition_counts[s][a][s_next] += 1
+        # self.reward_sums[s][a][s_next] += r
+        # prioriri =abs(r + (self.gamma * np.argmax(self.Q_sa[s_next]) - self.Q_sa[s,a]))
+        # if prioriri > self.priority_cutoff:
+        #     self.queue.put((-prioriri,(s,a)))
+        # #print(self.queue.qsize())
+        # for k in range(n_planning_updates):
+        #     if self.queue.empty():
+        #         break
+        #     _,(s_q,a_q) = self.queue.get()
+        #     prob_transition = (self.transition_counts[s][a])/(np.sum(self.transition_counts[s][a]))
+        #     s_next = np.random.choice(self.n_states, p=prob_transition)
+        #     r_new = self.reward_sums[s][a][s_next]/self.transition_counts[s][a][s_next]
+        #     self.Q_sa[s_q,a_q] += self.learning_rate * (r_new + self.gamma * np.argmax(self.Q_sa[s_next]) - self.Q_sa[s_q,a_q])
+        #     # find previous states and actions that lead to s_next
+        #     indices = np.where(self.transition_counts[:,:,s_q] > 0)
+        #     p_state_action_pairs = list(zip(indices[0], indices[1]))
+        #     for p_state, p_action in p_state_action_pairs:
+        #         #print(f"State: {p_state}, Action: {p_action}")
+        #         # if self.transition_counts[p_state][p_action][s_q]==0:
+        #         #     counter+=1
+        #         #     print("warnign!")#continue
+        #         r_bar = (self.reward_sums[p_state][p_action][s_q]/self.transition_counts[p_state][p_action][s_q])
+        #         prioriri1 = abs(r_bar + self.gamma * np.argmax(self.Q_sa[s_q]) - self.Q_sa[p_state,p_action])
+        #         if prioriri1 > self.priority_cutoff:
+        #             self.queue.put((-prioriri1,(p_state,p_action)))  
+        #print("Kaahrizmah, baby")
+        #print(counter)
+       
+        #works
         self.transition_counts[s][a][s_next] += 1
         self.reward_sums[s][a][s_next] += r
-        prioriri =abs((r + self.gamma * np.argmax(self.Q_sa[s_next]) - self.Q_sa[s,a]))
-        if prioriri > self.priority_cutoff:
-            self.queue.put((-prioriri,(s,a)))
-        #print(self.queue.qsize())
+        p = np.abs(r+(self.gamma*np.max(self.Q_sa[s_next])-self.Q_sa[s][a]))
+        if p > self.priority_cutoff:
+            self.queue.put((-p,(s,a)))
+        
         for k in range(n_planning_updates):
             if self.queue.empty():
                 break
-            _,(s,a) = self.queue.get()
-            prob_transition = (self.transition_counts[s][a])/(np.sum(self.transition_counts[s][a]))
-            s_next = np.random.choice(self.n_states, p=prob_transition)
-            r = self.reward_sums[s][a][s_next]/self.transition_counts[s][a][s_next]
-            self.Q_sa[s,a] += self.learning_rate * (r + self.gamma * np.argmax(self.Q_sa[s_next]) - self.Q_sa[s,a])
-            # find previous states and actions that lead to s_next
-            indices = np.where(self.transition_counts[:,:,s_next] > 0)
-            p_state_action_pairs = list(zip(indices[0], indices[1]))
-            for p_state, p_action in p_state_action_pairs: 
-                #print(f"State: {p_state}, Action: {p_action}")
-                if self.transition_counts[p_state][p_action][s]==0: 
-                    continue
-                r_bar = (self.reward_sums[p_state][p_action][s]/self.transition_counts[p_state][p_action][s])
-                prioriri1 = abs(r_bar + self.gamma * np.argmax(self.Q_sa[s]) - self.Q_sa[p_state,p_action])
-                if prioriri1 > self.priority_cutoff:
-                    self.queue.put((-prioriri1,(s,a)))  
-        print("Kaahrizmah, baby")
-        pass
+            _, (s, a) = self.queue.get()
+            prob_trans= self.transition_counts[s][a] / (np.sum(self.transition_counts[s][a]))
+            s_next = np.random.choice(self.n_actions, p=prob_trans)
+            r_bar = self.reward_sums[s][a][s_next] / self.transition_counts[s][a][s_next]
+            r = r_bar
+            self.Q_sa[s][a] = self.Q_sa[s][a] + self.learning_rate*(r + self.gamma * np.max(self.Q_sa[s_next]) - self.Q_sa[s][a])
+            for s_bar, a_bar in zip(*np.nonzero(self.transition_counts[:,:,s])[:2]):
+                r_hat = self.reward_sums[s_bar][a_bar][s] / self.transition_counts[s_bar][a_bar][s]
+                r_bar = r_hat
+                p = np.abs(r_bar+(self.gamma*np.max(self.Q_sa[s])-self.Q_sa[s_bar][a_bar]))
+                if p > self.priority_cutoff:
+                    self.queue.put((-p,(s_bar,a_bar)))
+            
+        
+
+       
+       
+
 
     def evaluate(self,eval_env,n_eval_episodes=30, max_episode_length=100):
         returns = []  # list to store the reward per episode
@@ -148,10 +189,13 @@ class PrioritizedSweepingAgent:
         mean_return = np.mean(returns)
         return mean_return        
 
+
 def test():
+
 
     n_timesteps = 10001 #10001
     gamma = 1.0
+
 
     # Algorithm parameters
     policy = 'ps' # or 'ps' 'dyna
@@ -159,11 +203,12 @@ def test():
     learning_rate = 0.2
     n_planning_updates = 3
 
+
     # Plotting parameters
     plot = True #True  
     plot_optimal_policy = True
     step_pause = 0.0001
-    
+   
     # Initialize environment and policy
     env = WindyGridworld()
     if policy == 'dyna':
@@ -172,33 +217,37 @@ def test():
         pi = PrioritizedSweepingAgent(env.n_states,env.n_actions,learning_rate,gamma) # Initialize PS policy
     else:
         raise KeyError('Policy {} not implemented'.format(policy))
-    
+   
     # Prepare for running
     s = env.reset()  
     continuous_mode = False
-    
+   
     for t in range(n_timesteps):            
         # Select action, transition, update policy
         a = pi.select_action(s,epsilon)
         s_next,r,done = env.step(a)
         pi.update(s=s,a=a,r=r,done=done,s_next=s_next,n_planning_updates=n_planning_updates)
-        
+       
         # Render environment
         if plot:
             env.render(Q_sa=pi.Q_sa,plot_optimal_policy=plot_optimal_policy,
                        step_pause=step_pause)
-            
+           
         #Ask user for manual or continuous execution
-        # if not continuous_mode:
-        #    key_input = input("Press 'Enter' to execute next step, press 'c' to run full algorithm")
-        #    continuous_mode = True if key_input == 'c' else False
+        if not continuous_mode:
+           key_input = input("Press 'Enter' to execute next step, press 'c' to run full algorithm")
+           continuous_mode = True if key_input == 'c' else False
+
 
         # Reset environment when terminated
         if done:
             s = env.reset()
         else:
             s = s_next
-            
-    
+           
+   
 if __name__ == '__main__':
     test()
+
+
+
